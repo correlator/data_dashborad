4acd0a96a513efcc3558873af6c3c11b9b4a3d93
Scott Hoch <scotthoch86@gmail.com>
2015-10-04 13:06:29 -0700

    show categories on home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :load_categories
 
   private
+
+  def load_categories
+    @categories = Category.all.order(:order)
+  end
 
   def authorize_superadmin
     if current_admin.nil? || !current_admin.super_admin
