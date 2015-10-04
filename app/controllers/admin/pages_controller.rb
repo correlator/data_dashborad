01702ada82e5b169d6227d71a094a644f24ad367
Scott Hoch <scotthoch86@gmail.com>
2015-10-04 14:25:04 -0700

    create edit page page

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -4,9 +4,22 @@ class Admin::PagesController < AdminController
     @page = Page.new
   end
 
+  def edit
+    @page = Page.find(params[:id])
+  end
+
   def create
     @page = Page.create(page_params)
     redirect_to admin_pages_path
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(page_params)
+      redirect_to admin_pages_path
+    else
+      render :edit
+    end
   end
 
   private
