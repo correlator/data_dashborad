ece1354b35d7ee4d8846467d90208aea33196813
Scott Hoch <scotthoch86@gmail.com>
2015-10-27 16:19:24 -0700

    can change graph width now

--- app/controllers/admin/external_graphs_controller.rb
+++ app/controllers/admin/external_graphs_controller.rb
@@ -14,8 +14,6 @@ class Admin::ExternalGraphsController < AdminController
 
   def update
     @external_graph = ExternalGraph.find(params[:id])
-    puts params
-    puts external_graph_params
     respond_to do |format|
       if @external_graph.update(external_graph_params)
         format.json { render json: @external_graph }
