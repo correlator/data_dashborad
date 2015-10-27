6a58f8926cf5fd9b07dea900b222929ab71b6585
Scott Hoch <scotthoch86@gmail.com>
2015-10-27 14:43:07 -0700

    string time on internal graphs

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,7 +1,7 @@
 class Point < ActiveRecord::Base
   belongs_to :line
   belongs_to :admin
-  default_scope { order(:time) }
+  default_scope { order(:position) }
 
   def created_by
     admin.email if admin
