9ffda0254f0568e888fa5d0e5fa4970febc8b810
Scott Hoch <scotthoch86@gmail.com>
2016-10-13 14:59:33 -0700

    fix migration

--- db/migrate/20161013204654_add_height_to_external_graph.rb
+++ db/migrate/20161013204654_add_height_to_external_graph.rb
@@ -2,9 +2,4 @@ class AddHeightToExternalGraph < ActiveRecord::Migration
   def change
     add_column :external_graphs, :height, :integer, default: 400;
   end
-
-  ExternalGraph.find_each do |graph|
-    graph.height = 400
-    graph.save
-  end
 end
